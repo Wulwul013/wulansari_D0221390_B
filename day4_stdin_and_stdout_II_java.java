63db916372a302587a8812efabe397ca55441601
Wulansari_D0221390_B <100018019+Wulwul013@users.noreply.github.com>
2022-02-24 18:04:56 +0700

    Add files via upload

---
+++ day4_stdin_and_stdout_II_java.java
@@ -0,0 +1,20 @@
+
+package wulan;
+
+import java.util.Scanner;
+
+
+public class day4_stdin_and_stdout_II_java {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+    
+}
